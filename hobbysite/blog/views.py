476fe02781644257967bc60574a8ad81d3e41909
loltreeman <naguio.greg@gmail.com>
2025-03-12 08:39:58 +0800

    Added list and detailed view to Articles

--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 from .models import Article, ArticleCategory
 
 # Create your views here 
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'article_list.html'
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article_detail.html'
+
