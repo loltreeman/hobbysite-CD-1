f15bb671a7e712e141807c49b72f643a01b5bd2f
fangaxeeeel <160501610+fangaxeeeel@users.noreply.github.com>
2025-03-12 19:36:45 +0800

    added models and views, added code for admin.py and urls.py, and created essential .html files

--- hobbysite/forum/models.py
+++ hobbysite/forum/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class PostCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = "Post Categories"
+    
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(PostCategory, on_delete=models.CASCADE, null=True, blank=True)
+    entry = models.TextField()
+    createdOn = models.DateTimeField(auto_now_add=True)
+    updatedOn = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['-createdOn']
+    
+    def __str__(self):
+        return self.title
