5b11c626453c4f1feaf3aeb6d7f8814a739febdd
Eurie-R <ramoy.uriel@gmail.com>
2025-03-13 01:49:56 +0800

    Fixed the url in settings

--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -19,6 +19,6 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("merchstore.urls"))
+    path('merchstore/', include("merchstore.urls"))
 ]
 
