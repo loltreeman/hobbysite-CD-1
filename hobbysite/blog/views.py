f05acd56e917ba40ea189317bb61b6a7d3fddfa4
loltreeman <naguio.greg@gmail.com>
2025-03-12 08:24:12 +0800

    Initial commit for blog app

---
+++ hobbysite/blog/views.py
@@ -0,0 +1,6 @@
+from django.views.generic import ListView, DetailView
+from django.http import HttpResponse 
+from django.shortcuts import render
+from .models import Article, ArticleCategory
+
+# Create your views here 
