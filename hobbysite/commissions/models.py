ab8d67664b1374c081fe027b000ebca59553ac99
Ritsu- <biasonneilaldous@gmail.com>
2025-03-13 18:44:35 +0800

    Fixed file naming conventions and syntax, also fixed minor errors.

--- hobbysite/commissions/models.py
+++ hobbysite/commissions/models.py
@@ -3,27 +3,32 @@ from django.db import models
 class Commission(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    peopleRequired = models.PositiveIntegerField()
-    createdOn = models.DateTimeField(auto_now_add=True)
-    updatedOn = models.DateTimeField(auto_now=True)
+    people_required = models.PositiveIntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
     
     class Meta:
-        ordering = ['createdOn']
+        ordering = ['created_on']
+        verbose_name = 'Commission'
+        verbose_name_plural = 'Commissions'
+        
 
 class Comment(models.Model):
     commission = models.ForeignKey(Commission, on_delete=models.CASCADE, related_name='comments')
     entry = models.TextField()
-    createdOn = models.DateTimeField(auto_now_add=True)
-    updatedOn = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.commission.title} comment - {self.createdOn.strftime('%d-%m-%y')}"
+        return f"{self.commission.title} comment - {self.created_on.strftime('%d-%m-%y')}"
     
     class Meta:
-        ordering = ['-createdOn']
+        ordering = ['-created_on']
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
 
 
 
