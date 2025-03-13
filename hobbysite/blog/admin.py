d380fb6b5758cbbef605e9ab241281bba8a68ae2
Ritsu- <biasonneilaldous@gmail.com>
2025-03-13 18:13:21 +0800

    remove redundant views functions, added ordering on the meta class as well as in the admin.py

--- hobbysite/blog/admin.py
+++ hobbysite/blog/admin.py
@@ -6,12 +6,14 @@ class ArticleCategoryAdmin(admin.ModelAdmin):
     model = ArticleCategory
     list_display = ['name',]
     search_display = ['name',]
+    ordering = ['name']
 
 class ArticleAdmin(admin.ModelAdmin):
     model = Article
     list_display = ['title', 'category', 'created_on', 'updated_on',]
     list_filter = ['category', 'created_on']
     search_fields = ['title', 'content',]
+    ordering = ['-created_on']
 
 admin.site.register(ArticleCategory, ArticleCategoryAdmin)
 admin.site.register(Article, ArticleAdmin)
--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -10,19 +10,3 @@ class ArticleListView(ListView):
 class ArticleDetailView(DetailView):
     model = Article
     template_name = 'article_detail.html'
-
-def article_list(request):
-    articles = Article.objects.all()
-    ctx = {"articles_list": articles}
-    return render(request, "article_list.html", ctx)
-
-def article(request, pk):
-    article = Article.objects.get(pk=pk)
-    ctx = {
-        "name": article.title,
-        "category": article.category.name,
-        "entry": article.entry,
-        "created_on": article.created_on,
-        "updated_on": article.updated_on,
-    }
-    return render(request, "article_detail.html", ctx)
