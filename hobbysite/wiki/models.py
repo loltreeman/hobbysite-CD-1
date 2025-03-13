6d84e8647228c04c79b1ed13a240d2a28c0e2386
Ritsu- <biasonneilaldous@gmail.com>
2025-03-13 18:51:02 +0800

    minor fix on syntax and improved consistency

--- hobbysite/wiki/models.py
+++ hobbysite/wiki/models.py
@@ -7,9 +7,9 @@ class ArticleCategory(models.Model):
     description = models.TextField()
 
     class Meta:
-        ordering = ["name"]
-        verbose_name = "Article Category"
-        verbose_name_plural = "Article Categories"
+        ordering = ['name']
+        verbose_name = 'Article Category'
+        verbose_name_plural = 'Article Categories'
 
     def __str__(self):
         return self.name
@@ -17,24 +17,19 @@ class ArticleCategory(models.Model):
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
-    category = models.ForeignKey(
-        ArticleCategory,
-        on_delete=models.SET_NULL,
-        null=True,
-        related_name="articles"
-    )
-    entry = models.TextField(blank=True, null=True)
+    category = models.ForeignKey(ArticleCategory, on_delete=models.SET_NULL, null=True, related_name='articles')
+    entry = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ["-created_on"]
-        verbose_name = "Article"
-        verbose_name_plural = "Articles"
+        ordering = ['-created_on']
+        verbose_name = 'Article'
+        verbose_name_plural = 'Articles'
 
     def __str__(self):
-        category = self.category.name if self.category else "Uncategorized"
+        category = self.category.name if self.category else 'Uncategorized'
         return f"{self.title} ({category})"
 
     def get_absolute_url(self):
-        return reverse("wiki:article_detail", kwargs={"pk": self.pk})
+        return reverse('wiki:article_detail', kwargs={'pk': self.pk})
