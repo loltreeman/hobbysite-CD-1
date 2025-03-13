7d6f7b893fbcf02aaf81633211c0afe0e1eb8715
Ritsu- <biasonneilaldous@gmail.com>
2025-03-13 18:28:08 +0800

    fix file naming conventions and syntax, fixed minor errors such as ordering and added a more detailed Meta class in each model for consistency in capitalization on the admin panel.

--- hobbysite/forum/admin.py
+++ hobbysite/forum/admin.py
@@ -4,10 +4,12 @@ from .models import PostCategory, Post
 class PostCategoryAdmin(admin.ModelAdmin):
     model = PostCategory
     list_display = ('name', 'description')
+    ordering = ['name']
 
 class PostAdmin(admin.ModelAdmin):
     model = Post
-    list_display = ('title', 'category__name', 'createdOn', 'updatedOn')
+    list_display = ('title', 'category__name', 'created_on', 'updated_on')
+    ordering = ['-created_on']
 
 admin.site.register(PostCategory, PostCategoryAdmin)
 admin.site.register(Post, PostAdmin)
--- hobbysite/forum/urls.py
+++ hobbysite/forum/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import threadDetail, threadList
 
-app_name = 'forum'
 
 urlpatterns = [
     path('threads/', threadList, name='threadList'),
     path('thread/<int:threadId>/', threadDetail, name='threadDetail'),
 ]
+
+
+app_name = 'forum'
