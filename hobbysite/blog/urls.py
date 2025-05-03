a205f04f7d568dcc5ba9a4d31d67417b0d032eb8
loltreeman <naguio.greg@gmail.com>
2025-05-03 13:56:37 +0800

    Added URL paths to have create and update view.

--- hobbysite/blog/urls.py
+++ hobbysite/blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ArticleListView, ArticleDetailView
+from .views import  ArticleListView, ArticleDetailView, ArticleCreateView, ArticleUpdateView
 
+app_name = 'blog'
 urlpatterns = [
     path('articles/', ArticleListView.as_view(), name='article_list'),
+    path('article/add/', ArticleCreateView.as_view(), name='article_add'),
     path('article/<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
+    path('article/<int:pk>/edit/', ArticleUpdateView.as_view(), name='article_edit'),
 ]
-
-app_name = "blog"
