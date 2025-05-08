cab74519b550c35330f21b26e662fdf33746a317
Ritsu- <biasonneilaldous@gmail.com>
2025-05-08 23:19:06 +0800

    refactored and finalized models.py

--- hobbysite/wiki/models.py
+++ hobbysite/wiki/models.py
@@ -1,61 +1,52 @@
 from django.db import models
-from datetime import datetime
-from django.urls import reverse
-from django.conf import settings
 from user_management.models import Profile
-
-# Create your models here.
+from django.urls import reverse
 
 class ArticleCategory(models.Model):
-    name = models.CharField(max_length = 255)
+    name = models.CharField(max_length=255)
     description = models.TextField()
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Article Category"
+        verbose_name_plural = "Article Categories"
 
     def __str__(self):
         return self.name
-    
-    
-    class Meta:
-        ordering = ['name']
-        verbose_name = 'Article Category'
-        verbose_name_plural = 'Article Categories'
 
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
-    category = models.ForeignKey(
-        'ArticleCategory', on_delete=models.SET_NULL, null=True, blank=True, related_name='articles'
-    )
+    author = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True, related_name="articles")
+    category = models.ForeignKey(ArticleCategory, on_delete=models.SET_NULL, null=True, blank=True, related_name="articles")
     entry = models.TextField()
+    header_image = models.ImageField(upload_to="wiki_headers/", blank=True, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
-    header_image = models.ImageField(upload_to='images/', blank=True, null=True)  # Ensure this field exists
-    author = models.ForeignKey(
-        'user_management.Profile', on_delete=models.SET_NULL, null=True, related_name='articles'
-    )
+
+    class Meta:
+        ordering = ["-created_on"]
+        verbose_name = "Article"
+        verbose_name_plural = "Articles"
 
     def __str__(self):
         return self.title
-    
-    def get_absolute_url(self):
-           return reverse('wiki:wiki-detail', args=[self.pk])
-    
 
-    class Meta:
-        ordering = ['-created_on']
-        verbose_name = 'Article'
-        verbose_name_plural = 'Articles'
+    def get_absolute_url(self):
+        return reverse("wiki:article-detail", args=[str(self.pk)])
+
 
 class Comment(models.Model):
-    author = models.ForeignKey('user_management.Profile', on_delete = models.SET_NULL, null = True, related_name = 'comments')
-    article = models.ForeignKey('Article', on_delete = models.CASCADE, related_name = 'comments')
+    author = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True, related_name="comments")
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="comments")
     entry = models.TextField()
-    created_on = models.DateTimeField(auto_now_add = True)
-    updated_on = models.DateTimeField(auto_now = True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ['created_on']
-        verbose_name = 'Comment'
-        verbose_name_plural = 'Comments'
+        ordering = ["created_on"]
+        verbose_name = "Comment"
+        verbose_name_plural = "Comments"
 
     def __str__(self):
-        return f"Comment by {self.author.user.profile} in {self.article.title}"
+        return f"Comment by {self.author} on {self.article}"
