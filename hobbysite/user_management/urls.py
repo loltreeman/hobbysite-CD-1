b9380670bda3affe17e888893662c575e6e9ee70
Christian Naguio <110177022+loltreeman@users.noreply.github.com>
2025-05-10 19:35:39 +0800

    Shortened the url path to not have profile, and instead use it in project level urls

--- hobbysite/user_management/urls.py
+++ hobbysite/user_management/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-    path('profile/', views.profile_dashboard, name='profile_dashboard'),
-    path('profile/edit/', views.profile_update, name='profile_update'),
+    path('dashboard/', views.profile_dashboard, name='profile_dashboard'),
+    path('edit/', views.profile_update, name='profile_update'),
     path('signup/', views.register_view, name='signup'),
 ]
 
