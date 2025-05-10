5b3abca1af79f7bee5eee93fcb0579bce6bf8dba
Christian Naguio <110177022+loltreeman@users.noreply.github.com>
2025-05-10 22:10:03 +0800

    Updated views to satisfy the global templates and user management

--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -4,22 +4,19 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from .models import Article, Comment, ArticleCategory
 from user_management.models import Profile 
-from .forms import CommentForm
+from .forms import ArticleForm, CommentForm
 
 class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
-    template_name = 'article_list.html'
+    template_name = 'blog/article_list.html'
     context_object_name = 'all_articles' 
 
     def get_queryset(self):
-        # return all articles except those by this user
         return Article.objects.exclude(author=self.request.user.profile).select_related('category')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # The ones you created:
         context['user_articles'] = Article.objects.filter(author=self.request.user.profile)
-        # Group the “others” (which come from get_queryset())
         grouped = {}
         for a in context['all_articles']:
             grouped.setdefault(a.category, []).append(a)
@@ -29,28 +26,22 @@ class ArticleListView(LoginRequiredMixin, ListView):
 
 class ArticleDetailView(DetailView):
     model = Article
-    template_name = 'article_detail.html'
+    template_name = 'blog/article_detail.html'
     context_object_name = 'article'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         article = self.object
 
-        context['related_articles'] = Article.objects.filter(
-            author=article.author
-        ).exclude(pk=article.pk)[:2]
+        context['related_articles'] = Article.objects.filter(author=article.author).exclude(pk=article.pk)[:2]
         context['images'] = article.images.all()
         context['comments'] = Comment.objects.filter(article=article).order_by('-created_on')
         context['comment_form'] = CommentForm()
         if self.request.user.profile == article.author:
             context['edit_link'] = True
 
-        context['previous_article'] = Article.objects.filter(
-            created_on__lt=article.created_on
-        ).order_by('-created_on').first()
-        context['next_article'] = Article.objects.filter(
-            created_on__gt=article.created_on
-        ).order_by('created_on').first()
+        context['previous_article'] = Article.objects.filter(created_on__lt=article.created_on).order_by('-created_on').first()
+        context['next_article'] = Article.objects.filter(created_on__gt=article.created_on).order_by('created_on').first()
         return context
 
     def post(self, request, *args, **kwargs):
@@ -63,32 +54,40 @@ class ArticleDetailView(DetailView):
             comment.save()
         return redirect('blog:article_detail', pk=article.pk)
 
-from .models import ArticleCategory  # make sure this import is at the top
 
 class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
-    fields = ['title', 'category', 'entry', 'header_image']
-    template_name = 'article_add.html'
+    form_class = ArticleForm
+    template_name = 'blog/article_add.html'
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        form.fields['category'].queryset = ArticleCategory.objects.all()
+        return form
+
+    def post(self, request, *args, **kwargs):
+        post_data = request.POST.copy()
+
+        new_category_name = post_data.get('new_category')
+        if new_category_name:
+            category, created = ArticleCategory.objects.get_or_create(name=new_category_name.strip())
+            post_data['category'] = category.id  
+
+        self.request.POST = post_data 
+
+        return super().post(request, *args, **kwargs)
 
     def form_valid(self, form):
         form.instance.author = self.request.user.profile
-
-        # Check if a new category was provided
-        new_category_name = self.request.POST.get('new_category')
-        if new_category_name:
-            category, created = ArticleCategory.objects.get_or_create(name=new_category_name.strip())
-            form.instance.category = category  # Override selected dropdown category
-
         return super().form_valid(form)
 
     def get_success_url(self):
         return reverse_lazy('blog:article_list')
 
-
 class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
     fields = ['title', 'category', 'entry', 'header_image']
-    template_name = 'article_edit.html'
+    template_name = 'blog/article_edit.html'
 
     def test_func(self):
         return self.request.user.profile == self.get_object().author
