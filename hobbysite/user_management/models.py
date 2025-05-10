d9408615b6a13b009a2494233b782363421b6d9e
Christian Naguio <110177022+loltreeman@users.noreply.github.com>
2025-05-10 19:35:21 +0800

    Changed the model name to display name for transparency

--- hobbysite/user_management/models.py
+++ hobbysite/user_management/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    username = models.CharField(max_length=63, verbose_name="Display name")
+    display_name = models.CharField(max_length=63, verbose_name="Display name")
     email = models.EmailField()
 
     def __str__(self):
-        return str(self.username)
+        return str(self.display_name)
