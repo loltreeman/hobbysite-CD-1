973fda7dbcae8ba873b42d9b9dbcfc7fb6382e0b
Christian Naguio <110177022+loltreeman@users.noreply.github.com>
2025-05-10 20:29:27 +0800

    Edited out a comment

--- hobbysite/blog/models.py
+++ hobbysite/blog/models.py
@@ -7,7 +7,7 @@ class ArticleCategory(models.Model):
     description = models.TextField()
 
     class Meta:
-        ordering = ['name']  # sorted by name ascending
+        ordering = ['name']  
         verbose_name = 'Article Category'
         verbose_name_plural = 'Article Categories'
 
@@ -34,7 +34,7 @@ class Article(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ['-created_on']  # newest first
+        ordering = ['-created_on']
         verbose_name = 'Article'
         verbose_name_plural = 'Articles'
 
@@ -61,14 +61,13 @@ class Comment(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ['created_on']  # oldest first
+        ordering = ['created_on']  
         verbose_name = 'Comment'
         verbose_name_plural = 'Comments'
 
     def __str__(self):
         return f"Comment by {self.author} on {self.article.title}" 
 
-# Article Image model for image gallery
 class ArticleImage(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='images')  # Link to Article
     image = models.ImageField(upload_to='blog/images/')  # Store images in 'blog/images' folder
