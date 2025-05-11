74171c72235a5f551cbb6aa7caeef47c01cd7ce9
Christian Naguio <110177022+loltreeman@users.noreply.github.com>
2025-05-11 22:55:10 +0800

    Configured these for deployment in Vercel

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
 MEDIA_URL = '/media/'  
 MEDIA_ROOT = BASE_DIR / 'media'
