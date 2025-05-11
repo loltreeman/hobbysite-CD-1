e315be533e81edbcf670a11c489a6a7bde73d218
fckjava <mattjoaquim37@gmail.com>
2025-05-11 11:08:53 -0700

    Try to make local profile to test and add navigation in the job page

--- hobbysite/commissions/models.py
+++ hobbysite/commissions/models.py
@@ -3,11 +3,10 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50, blank=True)
-    
+    display_name = models.CharField(max_length=50, blank=True)
+
     def __str__(self):
-        return self.name
-    
+        return self.display_name
 class Commission(models.Model):
     status_options = [
         ('0', 'Open'),
