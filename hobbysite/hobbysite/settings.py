f18d870a1a816d038ec15622497110680d533e6c
Ritsu- <biasonneilaldous@gmail.com>
2025-05-14 13:17:39 +0800

    Update settings.py

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['django-hobbysite-5yo65.ondigitalocean.app']
+ALLOWED_HOSTS = ['https://hobbysite-app-wf5xk.ondigitalocean.app/']
 
 # Application definition
 
