e85ee0f2eaf874c51d0dcfc50d646f4fd0cc0ba9
Ritsu- <biasonneilaldous@gmail.com>
2025-05-14 10:24:40 +0800

    Fix BASE_DIR and static files configuration

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -16,7 +16,7 @@ from urllib.parse import urlparse
 import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
